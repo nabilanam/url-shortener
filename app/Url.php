303dedd2efc7c1b22ff5d5966a6612766db18bc6
Nabil <nabilcoding@gmail.com>
2019-01-01 17:46:58 +0600

    add property typehint

--- app/Url.php
+++ app/Url.php
@@ -4,6 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int id
+ * @property string key
+ * @property string value
+ * @property int visited
+ */
 class Url extends Model
 {
     //
