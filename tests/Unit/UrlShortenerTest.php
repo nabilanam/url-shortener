bd8f95a701026f3e454c8158d17ccd14123de1d3
Nabil <nabilcoding@gmail.com>
2019-01-01 17:24:24 +0600

    add tests

--- tests/Unit/UrlShortenerTest.php
+++ tests/Unit/UrlShortenerTest.php
@@ -2,10 +2,57 @@
 
 namespace Tests\Unit;
 
+use App\Services\UrlShortener;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UrlShortenerTest extends TestCase
 {
 
+    /* @var UrlShortener */
+    private $shortener;
+
+    public function setUp()
+    {
+        $this->shortener = new UrlShortener();
+    }
+
+    public function testBaseIsSixtyTwo()
+    {
+        $expected = 62;
+        $found = $this->shortener->getBase();
+
+        self::assertEquals($expected, $found);
+    }
+
+    public function testEncoding()
+    {
+        $expected = [1, 33];
+        $found = $this->shortener->encode(95);
+
+        self::assertEquals($expected, $found);
+    }
+
+    public function testWrap()
+    {
+        $expected = 'Bh';
+        $found = $this->shortener->wrap([1, 33]);
+
+        self::assertEquals($expected, $found);
+    }
+
+    public function testDecoding()
+    {
+        $expected = [33, 1];
+        $found = $this->shortener->decode('Bh');
+
+        self::assertEquals($expected, $found);
+    }
+
+    public function testUnwrap()
+    {
+        $expected = 95;
+        $found = $this->shortener->unwrap([33, 1]);
+
+        self::assertEquals($expected, $found);
+    }
 }
